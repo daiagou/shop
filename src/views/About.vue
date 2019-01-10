1fe8317772600f14b04bc6b5a6bef825fb8810d5
abner <983351396@qq.com>
2019-01-10 18:34:17 +0800

    解决有空行的bug（加了一个div）

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,89 +1,14 @@
 <template>
-  <yd-layout>
-
-
-    <div>
-
-      <yd-scrolltab>
-        <yd-scrolltab-panel label="空调" icon="demo-icons-category1">
-          <yd-list theme="4">
-            <yd-list-item v-for="item, key in list" :key="key">
-              <img slot="img" :src="item.img">
-              <span slot="title">{{item.title}}</span>
-              <yd-list-other slot="other">
-                <div>
-                  <span class="list-price"><em>¥</em>{{item.price}}</span>
-                  <span class="list-del-price">¥{{item.w_price}}</span>
-                </div>
-                <div>content</div>
-              </yd-list-other>
-            </yd-list-item>
-          </yd-list>
-        </yd-scrolltab-panel>
-        <yd-scrolltab-panel label="冰箱" icon="demo-icons-category2" active>
-          <yd-list theme="4">
-            <yd-list-item v-for="item, key in list" :key="key">
-              <img slot="img" :src="item.img">
-              <span slot="title">{{item.title}}</span>
-              <yd-list-other slot="other">
-                <div>
-                  <span class="list-price"><em>¥</em>{{item.price}}</span>
-                  <span class="list-del-price">¥{{item.w_price}}</span>
-                </div>
-                <div>content</div>
-              </yd-list-other>
-            </yd-list-item>
-          </yd-list>
-        </yd-scrolltab-panel>
-        <yd-scrolltab-panel label="洗衣机" icon="demo-icons-category3">
-          <yd-list theme="4">
-            <yd-list-item v-for="item, key in list" :key="key">
-              <img slot="img" :src="item.img">
-              <span slot="title">{{item.title}}</span>
-              <yd-list-other slot="other">
-                <div>
-                  <span class="list-price"><em>¥</em>{{item.price}}</span>
-                  <span class="list-del-price">¥{{item.w_price}}</span>
-                </div>
-                <div>content</div>
-              </yd-list-other>
-            </yd-list-item>
-          </yd-list>
-        </yd-scrolltab-panel>
-        <!-- ... -->
-      </yd-scrolltab>
-
-    </div>
-
-
-
-    <yd-tabbar slot="tabbar">
-      <yd-tabbar-item title="首页" link="shop"  v-bind:active="$router.currentRoute.name=='Shop' ">
-        <yd-icon name="home" slot="icon" ></yd-icon>
-      </yd-tabbar-item>
-      <yd-tabbar-item title="购物车" link="shoppingCart" v-bind:active="$router.currentRoute.name=='ShoppingCart' ">
-        <yd-icon name="shopcart-outline" slot="icon"></yd-icon>
-      </yd-tabbar-item>
-      <yd-tabbar-item title="我的" link="me" v-bind:active="$router.currentRoute.name=='Me' ">
-        <yd-icon name="ucenter-outline" slot="icon"></yd-icon>
-      </yd-tabbar-item>
-    </yd-tabbar>
-
-  </yd-layout>
+  <yd-tab horizontal-scroll bg-color="transparent" >
+    <yd-tab-panel label="选项一">111111</yd-tab-panel>
+    <yd-tab-panel label="选项二">222222</yd-tab-panel>
+    <yd-tab-panel label="选项三">333333</yd-tab-panel>
+    <yd-tab-panel label="选项四">444444</yd-tab-panel>
+    <yd-tab-panel label="选项五">555555</yd-tab-panel>
+    <yd-tab-panel label="选项六">666666</yd-tab-panel>
+    <yd-tab-panel label="选项七">777777</yd-tab-panel>
+    <yd-tab-panel label="选项八">888888</yd-tab-panel>
+    <yd-tab-panel label="选项九">999999</yd-tab-panel>
+    <yd-tab-panel label="选项十">000000</yd-tab-panel>
+  </yd-tab>
 </template>
-<script>
-    export default {
-        data() {
-            return {
-                list: [
-                    {img: "//img1.shikee.com/try/2016/06/23/14381920926024616259.jpg", title: "标题111标题标题标题标题", price: 156.23, w_price: 89.36},
-                    {img: "//img1.shikee.com/try/2016/06/21/10172020923917672923.jpg", title: "标题222标题标题标题标题", price: 256.23, w_price: 89.36},
-                    {img: "//img1.shikee.com/try/2016/06/23/15395220917905380014.jpg", title: "标题333标题标题标题标题", price: 356.23, w_price: 89.36},
-                    {img: "//img1.shikee.com/try/2016/06/25/14244120933639105658.jpg", title: "标题444标题标题标题标题", price: 456.23, w_price: 89.36},
-                    {img: "//img1.shikee.com/try/2016/06/26/12365720933909085511.jpg", title: "标题555标题标题标题标题", price: 556.23, w_price: 89.36},
-                    {img: "//img1.shikee.com/try/2016/06/19/09430120929215230041.jpg", title: "标题666标题标题标题标题", price: 656.23, w_price: 89.36}
-                ]
-            }
-        }
-    }
-</script>
--- src/views/shop/Me.vue
+++ src/views/shop/Me.vue
@@ -10,77 +10,175 @@
          </yd-flexbox>
 
 
-         <yd-flexbox>
-             <yd-flexbox-item>
-                 待处理
-             </yd-flexbox-item>
-             <yd-flexbox-item>
-                 处理中
-             </yd-flexbox-item>
-             <yd-flexbox-item>
-                 已完成
-             </yd-flexbox-item>
-         </yd-flexbox>
+         <!--<yd-flexbox>-->
+             <!--<yd-flexbox-item>-->
+                 <!--待处理-->
+             <!--</yd-flexbox-item>-->
+             <!--<yd-flexbox-item>-->
+                 <!--处理中-->
+             <!--</yd-flexbox-item>-->
+             <!--<yd-flexbox-item>-->
+                 <!--已完成-->
+             <!--</yd-flexbox-item>-->
+         <!--</yd-flexbox>-->
 
+         <yd-tab bg-color="transparent">
+             <yd-tab-panel label="待处理">
 
-         <yd-flexbox style="margin-top: .5rem;margin-bottom: .5rem">
-             <yd-flexbox-item>
-                 <hr>
-             </yd-flexbox-item>
-         </yd-flexbox>
+                 <div style="border-radius:0.15rem;border: 0.02rem solid #000;margin: 0.25rem 0.5rem">
+                     <yd-flexbox style="height: 1.2rem">
+                         <div>
+                             <yd-icon name="shield-outline"></yd-icon>
 
+                         </div>
+                         <yd-flexbox-item>
+                             订单号:<i style="color: blue">123456789</i>
+                             <br>
 
-         <div style="border-radius:0.15rem;border: 0.02rem solid #000;margin: 0.25rem 0.5rem">
-             <yd-flexbox style="height: 1.2rem">
-                 <div>
-                     <yd-icon name="shield-outline"></yd-icon>
-
+                             <i style="font-size: .2rem"> 2018-12-12 12:12:12</i>
+                         </yd-flexbox-item>
+                         <yd-flexbox-item>
+                             <i style="color: red">￥123.00</i>
+                         </yd-flexbox-item>
+                     </yd-flexbox>
                  </div>
-                 <yd-flexbox-item>
-                     订单号:<i style="color: blue">123456789</i>
-                     <br>
+                 <div style="border-radius:0.15rem;border: 0.02rem solid #000;margin: 0.25rem 0.5rem">
+                     <yd-flexbox style="height: 1.2rem">
+                         <div>
+                             <yd-icon name="shield-outline"></yd-icon>
 
-                     <i style="font-size: .2rem"> 2018-12-12 12:12:12</i>
-                 </yd-flexbox-item>
-                 <yd-flexbox-item>
-                     <i style="color: red">￥123.00</i>
-                 </yd-flexbox-item>
-             </yd-flexbox>
-         </div>
-         <div style="border-radius:0.15rem;border: 0.02rem solid #000;margin: 0.25rem 0.5rem">
-             <yd-flexbox style="height: 1.2rem">
-                 <div>
-                     <yd-icon name="shield-outline"></yd-icon>
+                         </div>
+                         <yd-flexbox-item>
+                             订单号:<i style="color: blue">123456789</i>
+                             <br>
 
+                             <i style="font-size: .2rem"> 2018-12-12 12:12:12</i>
+                         </yd-flexbox-item>
+                         <yd-flexbox-item>
+                             <i style="color: red">￥123.00</i>
+                         </yd-flexbox-item>
+                     </yd-flexbox>
                  </div>
-                 <yd-flexbox-item>
-                     订单号:<i style="color: blue">123456789</i>
-                     <br>
+                 <div style="border-radius:0.15rem;border: 0.02rem solid #000;margin: 0.25rem 0.5rem">
+                     <yd-flexbox style="height: 1.2rem">
+                         <div>
+                             <yd-icon name="shield-outline"></yd-icon>
 
-                     <i style="font-size: .2rem"> 2018-12-12 12:12:12</i>
-                 </yd-flexbox-item>
-                 <yd-flexbox-item>
-                     <i style="color: red">￥123.00</i>
-                 </yd-flexbox-item>
-             </yd-flexbox>
-         </div>
-         <div style="border-radius:0.15rem;border: 0.02rem solid #000;margin: 0.25rem 0.5rem">
-             <yd-flexbox style="height: 1.2rem">
-                 <div>
-                     <yd-icon name="shield-outline"></yd-icon>
+                         </div>
+                         <yd-flexbox-item>
+                             订单号:<i style="color: blue">123456789</i>
+                             <br>
 
+                             <i style="font-size: .2rem"> 2018-12-12 12:12:12</i>
+                         </yd-flexbox-item>
+                         <yd-flexbox-item>
+                             <i style="color: red">￥123.00</i>
+                         </yd-flexbox-item>
+                     </yd-flexbox>
                  </div>
-                 <yd-flexbox-item>
-                     订单号:<i style="color: blue">123456789</i>
-                     <br>
 
-                     <i style="font-size: .2rem"> 2018-12-12 12:12:12</i>
-                 </yd-flexbox-item>
-                 <yd-flexbox-item>
-                     <i style="color: red">￥123.00</i>
-                 </yd-flexbox-item>
-             </yd-flexbox>
-         </div>
+             </yd-tab-panel>
+             <yd-tab-panel label="处理中">
+
+                 <div style="border-radius:0.15rem;border: 0.02rem solid #000;margin: 0.25rem 0.5rem">
+                     <yd-flexbox style="height: 1.2rem">
+                         <div>
+                             <yd-icon name="shield-outline"></yd-icon>
+
+                         </div>
+                         <yd-flexbox-item>
+                             订单号:<i style="color: blue">123456789</i>
+                             <br>
+
+                             <i style="font-size: .2rem"> 2018-12-12 12:12:12</i>
+                         </yd-flexbox-item>
+                         <yd-flexbox-item>
+                             <i style="color: red">￥123.00</i>
+                         </yd-flexbox-item>
+                     </yd-flexbox>
+                 </div>
+             </yd-tab-panel>
+
+             <yd-tab-panel label="已完成">
+                 <div style="border-radius:0.15rem;border: 0.02rem solid #000;margin: 0.25rem 0.5rem">
+                     <yd-flexbox style="height: 1.2rem">
+                         <div>
+                             <yd-icon name="shield-outline"></yd-icon>
+
+                         </div>
+                         <yd-flexbox-item>
+                             订单号:<i style="color: blue">123456789</i>
+                             <br>
+
+                             <i style="font-size: .2rem"> 2018-12-12 12:12:12</i>
+                         </yd-flexbox-item>
+                         <yd-flexbox-item>
+                             <i style="color: red">￥123.00</i>
+                         </yd-flexbox-item>
+                     </yd-flexbox>
+                 </div>
+             </yd-tab-panel>
+         </yd-tab>
+
+
+         <!--<yd-flexbox style="margin-top: .5rem;margin-bottom: .5rem">-->
+             <!--<yd-flexbox-item>-->
+                 <!--<hr>-->
+             <!--</yd-flexbox-item>-->
+         <!--</yd-flexbox>-->
+
+
+         <!--<div style="border-radius:0.15rem;border: 0.02rem solid #000;margin: 0.25rem 0.5rem">-->
+             <!--<yd-flexbox style="height: 1.2rem">-->
+                 <!--<div>-->
+                     <!--<yd-icon name="shield-outline"></yd-icon>-->
+
+                 <!--</div>-->
+                 <!--<yd-flexbox-item>-->
+                     <!--订单号:<i style="color: blue">123456789</i>-->
+                     <!--<br>-->
+
+                     <!--<i style="font-size: .2rem"> 2018-12-12 12:12:12</i>-->
+                 <!--</yd-flexbox-item>-->
+                 <!--<yd-flexbox-item>-->
+                     <!--<i style="color: red">￥123.00</i>-->
+                 <!--</yd-flexbox-item>-->
+             <!--</yd-flexbox>-->
+         <!--</div>-->
+         <!--<div style="border-radius:0.15rem;border: 0.02rem solid #000;margin: 0.25rem 0.5rem">-->
+             <!--<yd-flexbox style="height: 1.2rem">-->
+                 <!--<div>-->
+                     <!--<yd-icon name="shield-outline"></yd-icon>-->
+
+                 <!--</div>-->
+                 <!--<yd-flexbox-item>-->
+                     <!--订单号:<i style="color: blue">123456789</i>-->
+                     <!--<br>-->
+
+                     <!--<i style="font-size: .2rem"> 2018-12-12 12:12:12</i>-->
+                 <!--</yd-flexbox-item>-->
+                 <!--<yd-flexbox-item>-->
+                     <!--<i style="color: red">￥123.00</i>-->
+                 <!--</yd-flexbox-item>-->
+             <!--</yd-flexbox>-->
+         <!--</div>-->
+         <!--<div style="border-radius:0.15rem;border: 0.02rem solid #000;margin: 0.25rem 0.5rem">-->
+             <!--<yd-flexbox style="height: 1.2rem">-->
+                 <!--<div>-->
+                     <!--<yd-icon name="shield-outline"></yd-icon>-->
+
+                 <!--</div>-->
+                 <!--<yd-flexbox-item>-->
+                     <!--订单号:<i style="color: blue">123456789</i>-->
+                     <!--<br>-->
+
+                     <!--<i style="font-size: .2rem"> 2018-12-12 12:12:12</i>-->
+                 <!--</yd-flexbox-item>-->
+                 <!--<yd-flexbox-item>-->
+                     <!--<i style="color: red">￥123.00</i>-->
+                 <!--</yd-flexbox-item>-->
+             <!--</yd-flexbox>-->
+         <!--</div>-->
 
 
      </yd-layout>
