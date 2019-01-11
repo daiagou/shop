08aaa8cdfb5c4e907d126e4c19363b914ce953a3
abner <983351396@qq.com>
2019-01-11 13:14:01 +0800

    temp

--- src/views/shop/Me.vue
+++ src/views/shop/Me.vue
@@ -22,10 +22,10 @@
              <!--</yd-flexbox-item>-->
          <!--</yd-flexbox>-->
 
-         <yd-tab bg-color="transparent">
+         <yd-tab bg-color="">
              <yd-tab-panel label="待处理">
 
-                 <div style="border-radius:0.15rem;border: 0.02rem solid #000;margin: 0.25rem 0.5rem">
+                 <div style="border-radius:0.15rem;border: 0.02rem solid #000;margin: 0.25rem 0.5rem" @click="orderDetailDialogShow = true">
                      <yd-flexbox style="height: 1.2rem">
                          <div>
                              <yd-icon name="shield-outline"></yd-icon>
@@ -181,6 +181,112 @@
          <!--</div>-->
 
 
+
+         <!--订单明细对话框-->
+         <yd-popup v-model="orderDetailDialogShow" position="center" width="90%" >
+             <div style="background-color:#fff;">
+
+
+
+
+                 <yd-layout>
+                     <yd-flexbox style="height: 2rem;">
+                         <yd-flexbox-item>
+                             <i style="font-size: 0.8rem;color: red">￥28.20</i>
+                         </yd-flexbox-item>
+                     </yd-flexbox>
+
+
+                     <yd-flexbox style="margin-top: 0rem;">
+                         <div style="margin-left: .5rem">
+                            <i>订单号:</i>
+                         </div>
+                         <yd-flexbox-item>
+                            <i>12345678</i>
+                         </yd-flexbox-item>
+                     </yd-flexbox>
+
+                     <yd-flexbox style="margin-top: .3rem;">
+                         <div style="margin-left: .5rem">
+                            <i>收货人:</i>
+                         </div>
+                         <yd-flexbox-item>
+                            <i>西门飞雪</i>
+                         </yd-flexbox-item>
+                     </yd-flexbox>
+                     <yd-flexbox style="margin-top: .3rem;">
+                         <div style="margin-left: .5rem">
+                             <i>创建时间:</i>
+                         </div>
+                         <yd-flexbox-item>
+                             <i>2018-12-12 12:12:12</i>
+                         </yd-flexbox-item>
+                     </yd-flexbox>
+                     <yd-flexbox style="margin-top: .3rem;align-items:flex-start;margin-right: .5rem">
+                         <div style="margin-left: .5rem;">
+                             <i>收货地址:</i>
+                         </div>
+                         <yd-flexbox-item>
+                             <i>这里是收货地址这里是收货地址这里是收货地址这里是收货地址这里是收货地址这里是收货地址这里是收货地址</i>
+                         </yd-flexbox-item>
+                     </yd-flexbox>
+                     <yd-flexbox style="margin-top: .3rem;align-items:flex-start;margin-right: .5rem">
+                         <div style="margin-left: .5rem;">
+                             <i>备注:</i>
+                         </div>
+                         <yd-flexbox-item>
+                             <i>这里是备注内容这里是备注内容这里是备注内容这里是备注内容这里是备注内容这里是备注内容这里是备注内容这里是备注内容这里是备注内容</i>
+                         </yd-flexbox-item>
+                     </yd-flexbox>
+                     <yd-flexbox style="margin-top: .3rem;align-items:flex-start;margin-right: .5rem">
+                         <div style="margin-left: .5rem;">
+                             <i>订单明细:</i>
+                         </div>
+                         <yd-flexbox-item>
+                             <i>脸盆X1,脸盆X1,脸盆X1,脸盆X1,脸盆X1,脸盆X1,脸盆X1,脸盆X1,脸盆X1,脸盆X1,脸盆X1,垃圾桶X2.</i>
+                         </yd-flexbox-item>
+                     </yd-flexbox>
+
+
+
+                 </yd-layout>
+
+
+                 <!--<yd-cell-group >-->
+
+                     <!--<yd-cell-item >-->
+                         <!--<span slot="left" style="font-weight:bold">&nbsp;&nbsp;&nbsp;&nbsp;收货人：</span>-->
+                         <!--<yd-input slot="right" v-model="formData.name"  placeholder="请输入联系人名"></yd-input>-->
+                     <!--</yd-cell-item>-->
+
+                     <!--<yd-cell-item>-->
+                         <!--<span slot="left" style="font-weight:bold">手机号码：</span>-->
+                         <!--<yd-input slot="right" v-model="formData.phone"  placeholder="请输入手机号码"></yd-input>-->
+                     <!--</yd-cell-item>-->
+
+                     <!--<yd-cell-item>-->
+                         <!--<span slot="left" style="font-weight:bold">收货地址：</span>-->
+                         <!--<yd-input slot="right" v-model="formData.address"  placeholder="请输入收货地址"></yd-input>-->
+                     <!--</yd-cell-item>-->
+                     <!--<yd-cell-item>-->
+                         <!--<span slot="left" style="font-weight:bold">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;备注：</span>-->
+                         <!--<yd-input slot="right" v-model="formData.comment"  placeholder="老板有啥吩咐？"></yd-input>-->
+                     <!--</yd-cell-item>-->
+                 <!--</yd-cell-group>-->
+
+
+
+
+                 <p style="text-align: center;">
+                     <yd-button style="margin-right: .5rem" @click.native="orderDetailDialogShow = false" type="warning">取消订单</yd-button>
+                     <yd-button @click.native="orderDetailDialogShow = false">关闭</yd-button>
+                 </p>
+             </div>
+         </yd-popup>
+
+
+
+
      </yd-layout>
  </div>
 </template>
@@ -188,7 +294,12 @@
 <script>
     export default {
         data() {
-            return {}
+            return {
+
+                orderDetailDialogShow:false,
+
+
+            }
         }
     }
 </script>
--- src/views/shop/ShoppingCart.vue
+++ src/views/shop/ShoppingCart.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    <yd-layout v-if="isShopCartEmpty">
+    <yd-layout v-if="!isShopCartEmpty">
         <yd-flexbox style="height: 2rem;">
             <yd-flexbox-item >
                 <yd-icon name="shopcart"></yd-icon>
@@ -98,7 +98,7 @@
 
         <yd-flexbox>
             <yd-flexbox-item >
-                <yd-button size="small" type="primary">立即购买</yd-button>
+                <yd-button size="small" type="primary"  @click.native="buyDialogShow = true">立即购买</yd-button>
             </yd-flexbox-item>
         </yd-flexbox>
 
@@ -120,6 +120,40 @@
     </yd-layout>
 
 
+
+
+
+    <!--立即购买对话框-->
+    <yd-popup v-model="buyDialogShow" position="center" width="90%" >
+        <div style="background-color:#fff;">
+            <yd-cell-group >
+
+                <yd-cell-item >
+                    <span slot="left" style="font-weight:bold">&nbsp;&nbsp;&nbsp;&nbsp;收货人：</span>
+                    <yd-input slot="right" v-model="formData.name"  placeholder="请输入联系人名"></yd-input>
+                </yd-cell-item>
+
+                <yd-cell-item>
+                    <span slot="left" style="font-weight:bold">手机号码：</span>
+                    <yd-input slot="right" v-model="formData.phone"  placeholder="请输入手机号码"></yd-input>
+                </yd-cell-item>
+
+                <yd-cell-item>
+                    <span slot="left" style="font-weight:bold">收货地址：</span>
+                    <yd-input slot="right" v-model="formData.address"  placeholder="请输入收货地址"></yd-input>
+                </yd-cell-item>
+                <yd-cell-item>
+                    <span slot="left" style="font-weight:bold">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;备注：</span>
+                    <yd-input slot="right" v-model="formData.comment"  placeholder="老板有啥吩咐？"></yd-input>
+                </yd-cell-item>
+            </yd-cell-group>
+            <p style="text-align: center;">
+                <yd-button @click.native="buyDialogShow = false">确认购买</yd-button>
+            </p>
+        </div>
+    </yd-popup>
+
+
 </div>
 </template>
 
@@ -127,8 +161,15 @@
     export default {
         data() {
             return {
-                isShopCartEmpty:true,
+                isShopCartEmpty:false,
+                buyDialogShow:false,
+                formData:{
+                    name:'',
+                    phone:'',
+                    address:'',
+                    comment:'',
 
+                },
             }
         }
     }
