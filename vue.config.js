2853597def8fc6cd9c8b1d322dbd5cf9c9d6374b
abner <983351396@qq.com>
2019-01-25 17:56:46 +0800

    完成

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+
+
+
+module.exports = {
+    // 基本路径
+    baseUrl: '',
+    // 输出文件目录
+    outputDir: 'dist',
+    // 生产环境是否生成 sourceMap 文件
+    productionSourceMap: true,
+    // webpack-dev-server 相关配置
+    devServer: {
+        port: 8888,
+    },
+}
+
+
